7d1d09178a6bbb9e6d832570509f6e876c19541a
“Andres <andres@fulfilld.io>
2024-03-09 12:07:26 -0600

    uploading

--- wdio.testingbot.mobile.conf.ts
+++ wdio.testingbot.mobile.conf.ts
@@ -1,4 +1,3 @@
-
 export const config = {
     user: process.env.TB_KEY,
     key: process.env.TB_SECRET,
@@ -96,7 +95,7 @@ export const config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    "baseUrl": "./trasureplanet",
+    // baseUrl: 'http://localhost',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 50000,
